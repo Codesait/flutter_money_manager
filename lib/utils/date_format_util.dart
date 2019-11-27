df1f0499e4871b93b5f3190630de48d0f3a3a1d6
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2019-11-27 08:09:01 +0630

    - At date_format_util.dart, add new helper methods.

--- lib/utils/date_format_util.dart
+++ lib/utils/date_format_util.dart
@@ -8,8 +8,24 @@ String shortDateFormat(DateTime date) {
   return DateFormat('EEE, MMM d, yyyy').format(date);
 }
 
+String shortDateFormatWithoutDay(DateTime date) {
+  return DateFormat('MMMM yyyy').format(date);
+}
+
+String shortDateFormatWithoutMonthAndDay(DateTime date) {
+  return DateFormat('yyyy').format(date);
+}
+
 DateTime getDateWithoutTime(DateTime date) {
   return new DateTime(date.year, date.month, date.day);
+}
+
+DateTime getDateWithoutDayAndTime(DateTime date) {
+  return new DateTime(date.year, date.month);
+}
+
+DateTime getDateWithoutMonthAndDayAndTime(DateTime date) {
+  return new DateTime(date.year);
 }
 
 String convertToISO8601DateFormat(DateTime date) {
