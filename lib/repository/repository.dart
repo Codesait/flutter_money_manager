c2ef30e7e04d82fc6d4cbd781e639a5cdf68b032
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2019-12-03 22:48:20 +0630

    - Add repository.dart. At Home page, refactored UI design.

---
+++ lib/repository/repository.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_money_manager/storage_factory/database/transaction_table.dart';
+
+class Repository {
+  final TransactionTable _transactionTable = TransactionTable();
+
+  Future<double> getTotalBalance() => _transactionTable.getTotalBalance();
+}
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,149 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_money_manager/enums/transaction_filter_type.dart';
 import 'package:flutter_money_manager/enums/transaction_type.dart';
-import 'package:flutter_money_manager/models/total.dart';
-import 'package:flutter_money_manager/storage_factory/database/transaction_table.dart';
-import 'package:flutter_money_manager/tiles/summary_tile.dart';
+import 'package:flutter_money_manager/models/list_item.dart';
+import 'package:flutter_money_manager/repository/repository.dart';
 import 'package:flutter_money_manager/utils/number_format_util.dart';
+import 'package:flutter_money_manager/widgets/color_circle.dart';
+import 'package:flutter_money_manager/widgets/future_list_item_builder.dart';
 
 class Home extends StatelessWidget {
-  double getTotalBalance(List<Total> totals) {
-    double totalBalance = 0;
-    for (Total total in totals) {
-      if (total.type == TransactionType.INCOME) {
-        totalBalance += total.amount;
-      } else {
-        totalBalance -= total.amount;
-      }
-    }
-    return totalBalance;
-  }
-
-  List<Widget> _buildChildren(BuildContext context, List<Total> totals) {
-    return <Widget>[
-      Expanded(
-        child: Center(
-          child: _buildTotalBalance(
-            context,
-            getTotalBalance(totals),
+  Widget _totalBalanceBuilder(BuildContext context, double totalBalance) {
+    return Container(
+      height: 150.0,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            standardNumberFormat(totalBalance),
+            style: Theme.of(context).textTheme.display2.copyWith(
+                  color: Colors.white,
+                ),
           ),
-        ),
+          Text(
+            'Total Balance',
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ],
       ),
-      Expanded(
-        child: _buildSubTotals(
-          context,
-          totals,
-        ),
-      ),
-    ];
-  }
-
-  Widget _buildPortrait(BuildContext context, List<Total> totals) {
-    return Column(children: _buildChildren(context, totals));
-  }
-
-  Widget _buildLandscape(BuildContext context,
-      double appBarHeight,
-      List<Total> totals,) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: appBarHeight),
-      child: Row(children: _buildChildren(context, totals)),
     );
   }
 
-  Widget _buildTotalBalance(BuildContext context, double amount) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text(
-          standardNumberFormat(amount),
-          style: Theme
-              .of(context)
-              .textTheme
-              .display2
-              .copyWith(
-            color: Colors.white,
-          ),
-        ),
-        Text(
-          'Total Balance',
-          style: Theme
-              .of(context)
-              .textTheme
-              .caption,
-        ),
-      ],
+  Widget _listItemBuilder(List<ListItem> items) {
+    return Scrollbar(
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          TransactionItem item = items[index] as TransactionItem;
+          double amount = item.transaction.amount;
+          if (item.transaction.category.transactionType ==
+              TransactionType.EXPENSE) {
+            amount *= -1;
+          }
+          return ListTile(
+            leading: ColorCircle(color: item.transaction.category.color),
+            title: Text(
+              item.transaction.category.name,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            trailing: Text(
+              standardNumberFormat(amount),
+              style: Theme.of(context).textTheme.caption,
+            ),
+            onTap: () => print(item.transaction.category.name),
+          );
+        },
+        itemCount: items.length,
+      ),
     );
   }
 
-  Widget _buildSubTotals(BuildContext context, List<Total> totals) {
-    Total incomeTotal, expenseTotal;
-    for (Total total in totals) {
-      if (total.type == TransactionType.INCOME) {
-        incomeTotal = total;
-      } else {
-        expenseTotal = total;
-      }
-    }
-
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Divider(),
-        SummaryTile(
-          onTap: () {
-            // TODO : go to report with "INCOME" filter
-          },
-          iconData: Icons.trending_up,
-          color: Colors.green,
-          amount: incomeTotal != null ? incomeTotal.amount : 0,
-          label: 'Total Income',
-        ),
-        Divider(),
-        SummaryTile(
-          onTap: () {
-            // TODO : go to report with "EXPENSE" filter
-          },
-          iconData: Icons.trending_down,
-          color: Colors.red,
-          amount: expenseTotal != null ? expenseTotal.amount : 0,
-          label: 'Total Expense',
-        ),
-        Divider(),
-      ],
+  Widget _futureTotalBalanceBuilder() {
+    return FutureBuilder(
+      future: Repository().getTotalBalance(),
+      builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
+        double totalBalance = snapshot.hasData ? snapshot.data : 0.0;
+        return _totalBalanceBuilder(context, totalBalance);
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: FutureBuilder(
-        future: TransactionTable().getTotals(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          } else if (snapshot.hasData) {
-            return OrientationBuilder(
-              builder: (context, orientation) {
-                if (orientation == Orientation.portrait) {
-                  return _buildPortrait(
-                    context,
-                    snapshot.data,
-                  );
-                } else {
-                  return _buildLandscape(
-                    context,
-                    AppBar().preferredSize.height,
-                    snapshot.data,
-                  );
-                }
-              },
-            );
-          } else {
-            return Center(child: new CircularProgressIndicator());
-          }
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        _futureTotalBalanceBuilder(),
+        Expanded(
+          child: FutureListItemBuilder(
+            transactionFilterType: TransactionFilterType.ALL,
+            loadingBuilderFn: (BuildContext context1) {
+              return CircularProgressIndicator();
+            },
+            listItemBuilderFn: (
+              BuildContext context2,
+              List<ListItem> listItems,
+            ) {
+              return _listItemBuilder(listItems);
+            },
+            emptyListItemBuilderFn: (BuildContext context3) {
+              return null;
+            },
+            errorBuilderFn: (BuildContext context4, String error) {
+              return Center(
+                child: Text(error),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
